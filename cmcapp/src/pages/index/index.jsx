eca83e46a5b9d7a497ca1f95374305abc7a91a06
Auly <whitefryfish@gmail.com>
2019-01-14 00:15:10 +0800

    fix page bug

--- cmcapp/src/pages/index/index.jsx
+++ cmcapp/src/pages/index/index.jsx
@@ -1,6 +1,7 @@
 import Taro from '@tarojs/taro'
 import {View, Text, Button} from '@tarojs/components'
-import './index.less'
+import {AtButton} from 'taro-ui'
+import './index.scss'
 
 const APP = Taro.getApp();
 
@@ -18,39 +19,45 @@ export default class Index extends Taro.Component {
   }
 
   componentWillMount() {
-    Taro.getUserInfo({
-      success: res => {
-        APP.globalData = {
-          userInfo: res.userInfo
-        };
+
+    Taro.getUserInfo()
+    .then(res => {
+      APP.globalData = {
+        userInfo: res.userInfo
+      };
+      this.setState({
+        nickName: res.userInfo.nickName
+      });
+      let option = {
+        url: 'http://localhost:8080/hello',
+        method: 'POST',
+        data: {name: res.userInfo.nickName}
+      };
+      Taro.request(option)
+      .then(res => {
         this.setState({
-          nickName: res.userInfo.nickName
+          number: res.data.number
         });
-        Taro.request({
-          url: 'http://localhost:8080/hello',
-          method: 'POST',
-          data: {name: res.userInfo.nickName}
-        }).then(res => {
-          this.setState({number: res.data.number})
-        })
-      },
-
-      fail: () => {
-        this.setState({userInfo: 'Guest'})
-      }
-
+      })
+      .catch(err => {
+        console.log('newErr', err);
+      })
     })
+    .catch(err => {
+      console.log('userErr', err);
+    });
   }
 
   render() {
     return (
-      <View>
-        <Text>Hello, {this.state.nickName}: </Text>
+      <View className={'at-article'}>
+        <View className={'at-article__h1'}>Hello, {' ' + this.state.nickName}: </View>
         {this.state.number === undefined ?
-          <Button openType={'getUserInfo'}
-                  onGetUserInfo={this.componentWillMount}>Login</Button>
+          <AtButton type={'primary'} circle={true} openType={'getUserInfo'}
+                    onGetUserInfo={this.componentWillMount}>Login</AtButton>
           :
-          <Text>You are the&nbsp;{this.state.number}&nbsp;one to login</Text>
+          <View className={'at-article__p'}>You are the{' ' + this.state.number}th user to
+            login</View>
         }
       </View>
     )
