fe87341f146b315460ab738e384ed7e8bb1a7b76
Auly <whitefryfish@gmail.com>
2019-02-13 00:36:05 +0800

    change using typescript

--- cmcapp/src/app.tsx
+++ cmcapp/src/app.tsx
@@ -1,4 +1,4 @@
-import Taro, { Component } from '@tarojs/taro'
+import Taro, { Component, Config } from '@tarojs/taro'
 import Index from './pages/index'
 
 import './app.scss'
@@ -11,7 +11,14 @@ import './app.scss'
 
 class App extends Component {
 
-  config = {
+  /**
+   * 指定config的类型声明为: Taro.Config
+   *
+   * 由于 typescript 对于 object 类型推导只能推出 Key 的基本类型
+   * 对于像 navigationBarTextStyle: 'black' 这样的推导出的类型是 string
+   * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
+   */
+  config: Config = {
     pages: [
       'pages/index/index'
     ],
